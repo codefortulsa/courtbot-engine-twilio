67df61e41606fe8f7e8ea051dbb07eb32650d60a
Blixa Morgan <blixa@projectmakeit.com>
2017-01-29 16:30:43 -0600

    add tests from courtbot-engineg

---
+++ test/setup.js
@@ -0,0 +1,29 @@
+import chai from "chai";
+import dirtyChai from "dirty-chai";
+import chaiAsPromised from "chai-as-promised";
+import sinonChai from "sinon-chai";
+import sinon from "sinon";
+import Chance from "chance";
+import chaiEventEmitter from "chai-eventemitter"
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+chai.use(chaiEventEmitter);
+chai.use(dirtyChai);
+
+function setup() {
+    const sandbox = sinon.sandbox.create();
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    return {
+        expect: chai.expect,
+        should: chai.should(),
+        chance: new Chance(),
+        sandbox
+    };
+}
+
+export default setup;
